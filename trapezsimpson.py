90d4cd34b2215b106b26e1b87d60c7c6afa9fee9
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2022-06-08 12:22:29 +0300

    change for trapez

--- trapezsimpson.py
+++ trapezsimpson.py
@@ -1,18 +1,41 @@
 import math
 
 import sympy as sp
+from sympy.utilities.lambdify import lambdify
+x = sp.symbols('x')
+f = x**4 + x**3 - 3*x**2
+first_derivative = sp.diff(f, x)
+second_derivative = sp.diff(first_derivative,x)
+print(f)
+import math
+print(first_derivative)
+print(second_derivative)
+second_derivative = lambdify(x,second_derivative)
+values = [second_derivative(x) for x in range(-100,101)]
+print(max(values))
+epsilon = 0.000002
 
+
+def find_number_of_partitions(start,end):
+    value = epsilon*(12/(((end-start)**3)*10))
+    return math.sqrt(1/value)
+
+
+
+#print(find_number_of_partitions(0,math.pi))
 
 def trapez(f,a,b,n):
     I = 0
     result = 0
     i = 0
-    size = (b-a)/n
+    size = (b-a)/n    # גודל המקטע
     while i < n:
         result += (1/2)*(size)*(f(a)+f(a+size))
         a +=size
         i +=1
     return result
+
+
 
 def simpson(f,a,b,n):
     h = (b-a)/n
@@ -35,5 +58,6 @@ def simpson(f,a,b,n):
 
 
 f = lambda x:sp.sin(x)
-print(trapez(f,0,math.pi,4))
+print(trapez(f,0,math.pi,find_number_of_partitions(0,math.pi)))
 print(simpson(f,0,math.pi,4))
+
